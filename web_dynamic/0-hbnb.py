518e836d9be883179fa21dd7f322533580f47f80
Messih Grmay <mesihgrmay@yahoo.com>
2022-11-27 11:24:18 -0800

    Task 1

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -35,7 +35,7 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    cache_id = uuid.uuid4() 
+    cache_id = uuid.uuid4()
 
     return render_template('0-hbnb.html',
                            cache_id=cache_id,
